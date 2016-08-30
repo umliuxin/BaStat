4e53929d3efdcc5310f4a736b9aec038d4a07046
Xin Liu <umliuxin@umich.edu>
2016-08-30 02:06:02 -0700

    reset table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require materialize.min
-//= require custom_bootgrid
 //= require turbolinks
--- app/assets/javascripts/design_guide.js
+++
@@ -1,3 +0,0 @@
-$('#bootgrid-1').bootgrid();
-$('.dropdown-toggle').dropdown();
-console.log($('.dropdown-toggle'));
