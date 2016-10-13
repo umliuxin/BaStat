bd514040c0124e1a4f8958eb49fa03802899a1a5
Xin Liu <umliuxin@umich.edu>
2016-10-12 21:18:17 -0700

    add header data

--- app/models/team.rb
+++ app/models/team.rb
@@ -12,25 +12,44 @@ class Team
 
   def self.build
     @team = Team.new
-    @team.current_season
-    @game_win = @team.game_win
-    @game_lose = @team.game_lose
+    @team.current_season = @team.current_season
+    @team.game_win = @team.game_win
+    @team.game_lose = @team.game_lose
+    @team.next_game = @team.next_game
+    @team.last_game = @team.last_game
+    ap @team.current_season_avg_stat
     @team
   end
 
   def current_season
-    @current_season ||= Season.get_current_season
+    Season.get_current_season
   end
 
   def game_win
-    Score.joins(:game).where("point_total > point_op_total AND recording_quarter = 5 AND games.season_id = ?", @current_season.id).count
+    Score.joins(:game).where("point_total > point_op_total AND recording_quarter = 5 AND games.season_id = ?", self.current_season.id).count
   end
 
   def game_lose
-    Score.joins(:game).where("point_total < point_op_total AND recording_quarter = 5 AND games.season_id = ?", @current_season.id).count
+    Score.joins(:game).where("point_total < point_op_total AND recording_quarter = 5 AND games.season_id = ?", self.current_season.id).count
   end
 
+  def next_game
+    Game.order("gametime").find_by('gametime > ? AND game_record = ?', DateTime.now ,false)
+  end
 
+  def last_game
+    Game.order("gametime DESC").find_by('gametime < ? AND game_record = ?', DateTime.now, true)
+  end
+
+  def current_season_avg_stat
+
+    sql = "SELECT AVG(fgm) AS fgm, AVG(fga) AS fga, AVG(tpa) AS tpa, AVG(tpm) AS tpm, AVG(fta) AS fta, AVG(ftm) AS ftm, AVG(oreb) AS oreb, AVG(dreb) AS dreb, AVG(ast) AS ast
+FROM team_stats
+LEFT JOIN games
+ON team_stats.game_id=games.id
+WHERE games.game_record ='t' AND games.season_id="+self.current_season.id.to_s
+    TeamStat.find_by_sql(sql).first
+  end
 
 
 end
