324f693e988b7be36e3bc5d2a2ad80d2cfa37e8a
Xin Liu <umliuxin@umich.edu>
2016-10-17 23:23:43 -0700

    patch: empty player page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,9 +4,5 @@ class PlayersController < ApplicationController
   end
   def show
     @player = Player.build(params[:id])
-    # @player = Player.find(params[:id])
-    # @player.avg_stat_current_season = @player.current_season_avg_stat
-    # @player.avg_stat_seasons = @player.seasons_avg_stat
-    # @player.games_current_season = @player.current_season_games
   end
 end
